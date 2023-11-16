e9e6c63edc947d6b014cb704b69f01c83c2b5939
robeltade <robeltade37@gmail.com>
2023-11-15 17:20:48 -0800

    js warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const argc = process.argv.length;
+const numArgs = process.argv.length - 2;
 
-if (argc > 2) {
-  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
-} else {
+if (numArgs === 0) {
   console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
